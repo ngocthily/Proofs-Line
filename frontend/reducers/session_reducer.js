d9f8265a2ab277fe6f1242dad116fa68e23ffb26
ngocthily <ngoctly94@gmail.com>
2020-04-29 20:48:28 -0700

    Prepare for initial Heroku push

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -8,12 +8,9 @@ const _nullUser = Object.freeze({
 });
 
 const sessionReducer = (state = _nullUser, action) => {
-    // debugger
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            // whyyyyyyy
-            // not hitting receive current user case
             return { id: action.currentUser.id };
         case LOGOUT_CURRENT_USER:
             return _nullUser;
