712a15adc65f8de55141d428306563ce74491b1d
ngocthily <ngoctly94@gmail.com>
2020-06-22 15:13:57 -0700

    Finished CSS for splash, login, and signup page

--- frontend/components/nav_bar/home_nav_bar.jsx
+++ frontend/components/nav_bar/home_nav_bar.jsx
@@ -5,7 +5,7 @@ export default ({ currentUser, logout }) => {
     function dropdownChange() {
             $('.dropdown-icon').toggleClass('active');
     }
-
+    
     const display = currentUser ? (
         <div className="navbar-logout-btn-container">
             {/* current user has no username, just id so no username shows up
@@ -23,57 +23,57 @@ export default ({ currentUser, logout }) => {
 
     return (
     <div className="home-nav-bar-container-wrapper">
-    <div className="nav-bar-container">
-        <div className="nav-bar-header">
-            <div className="dropdown">
-                <div className="dropdown-items">
-                    <div className="dropdown-icon" onClick={dropdownChange}>
-                        <div className="x-icon">
-                        <div className="dropdown-content">
-                            <p></p>
-                            <div className ="dropdown-home">
-                                <Link to="/" className="dropdown-home-link">
-                                    <p className="home-word">Home</p>
-                                </Link>
+        <div className="nav-bar-container">
+            <div className="nav-bar-header">
+                <div className="dropdown">
+                    <div className="dropdown-items">
+                        <div className="dropdown-icon" onClick={dropdownChange}>
+                            <div className="x-icon">
+                            <div className="dropdown-content">
+                                <p></p>
+                                <div className ="dropdown-home">
+                                    <Link to="/" className="dropdown-home-link">
+                                        <p className="home-word">Home</p>
+                                    </Link>
+                                </div>
+                                <p className="dropdown-public">PUBLIC</p>
+                                <div className="dropdown-question">
+                                    <Link to="/questions" className="dropdown-question-link">
+                                        <p className="dropdown-questions-word">Questions</p>
+                                    </Link>
+                                </div>
+                                <div className="dropdown-jobs">
+                                    <Link to="/jobs" className="dropdown-jobs-link">
+                                        <p className="dropdown-jobs-word">Jobs</p>
+                                    </Link>
+                                </div>
                             </div>
-                            <p className="dropdown-public">PUBLIC</p>
-                            <div className="dropdown-question">
-                                <Link to="/questions" className="dropdown-question-link">
-                                    <p className="dropdown-questions-word">Questions</p>
-                                </Link>
                             </div>
-                            <div className="dropdown-jobs">
-                                <Link to="/jobs" className="dropdown-jobs-link">
-                                    <p className="dropdown-jobs-word">Jobs</p>
-                                </Link>
-                            </div>
-                        </div>
                         </div>
                     </div>
                 </div>
+                <div className="home-logo">
+                    <Link to="/">
+                        <img className="home-logo-img" src={window.logo} />
+                    </Link>
+                </div>
             </div>
-            <div className="home-logo">
-                <Link to="/">
-                    <img className="home-logo-img" src={window.logo} />
-                </Link>
+            {/* <div className="space1"></div>
+            <div className="space2"></div> */}
+            {/* <div className="nav-bar-buttons-wrapper"> */}
+            <div className="nav-bar-buttons">
+                {/* <div className="search-img">
+                    <i className="fas fa-search"></i>
+                </div>
+                    <input className="search-bar" type="text" placeholder="Search..(coming)"></input> */}
+                <div>
+                    {/* <Link to='/login'><button type="button" className="btn-login">Log in</button></Link>
+                    <Link to='/signup'><button type="button" className="btn-signup">Sign up</button></Link> */}
+                    {display}
+                </div>
+            {/* </div> */}
             </div>
         </div>
-        {/* <div className="space1"></div>
-        <div className="space2"></div> */}
-        {/* <div className="nav-bar-buttons-wrapper"> */}
-        <div className="nav-bar-buttons">
-            <div className="search-img">
-                <i className="fas fa-search"></i>
-            </div>
-                <input className="search-bar" type="text" placeholder="Search..(coming)"></input>
-            <div>
-                {/* <Link to='/login'><button type="button" className="btn-login">Log in</button></Link>
-                <Link to='/signup'><button type="button" className="btn-signup">Sign up</button></Link> */}
-                {display}
-            </div>
-        {/* </div> */}
-        </div>
-    </div>
     </div>
     )
 }
--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaGlobeAmericas } from "react-icons/fa";
 
-export default () => (
-    <div className="sidebar">
-        <Link to="/" className="sidebar-home-link">
-            <p className="sidebar-home">Home</p>
-        </Link>
-        <p className="sidebar-public">PUBLIC</p>
-        <div className="sidebar-title">
-            <Link to="/questions" className="sidebar-title-link">
-                <p className="sidebar-title-word">Proofs Line</p>
-            </Link>
-        </div>
-        <Link to="/jobs" className="sidebar-job-link">
-            <p className="sidebar-jobs">Jobs</p>
-        </Link>
-    </div>
-)
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <Link to="/" className="sidebar-home-link">
+                    <p className="sidebar-home">Home</p>
+                </Link>
+                <p className="sidebar-public">PUBLIC</p>
+                <div className="sidebar-title">
+                    <Link to="/questions" className="sidebar-title-link">
+                        <p className="sidebar-title-word">
+                            <FaGlobeAmericas /> &nbsp;
+                            Proofs Line
+                        </p>
+                    </Link>
+                </div>
+                <Link to="/jobs" className="sidebar-job-link">
+                    <p className="sidebar-jobs">Jobs</p>
+                </Link>
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
