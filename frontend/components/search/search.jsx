3ce6b988df7d0fbbf757104dd6066340a338865e
ngocthily <ngoctly94@gmail.com>
2020-06-25 03:26:43 -0700

    Added vote and answer count to search results

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -71,6 +71,35 @@ class Search extends React.Component {
     }
 
     render() {
+        const timeAgo = (secs) => (
+            (secs < 60) ?
+                `asked ${Math.round(secs)} secs ago` :
+            (secs === 60) ?
+                `asked 1 min ago` :
+            (secs < 3600) ?
+                `asked ${Math.round(secs / 60)} mins ago` :
+            (secs === 3600) ?
+                `asked 1 hour ago` :
+            (secs < 86400) ?
+                `asked ${Math.round(secs / 3600)} hours ago` :
+                `asked ${Math.round(secs / 86400)} days ago`
+        );
+
+        const countOfVotes = (questionVotes) => {
+            let numOfUpvotes = 0;
+            let numOfDownvotes = 0;
+            for (let i = 0; i < questionVotes.length; i++) {
+                if (questionVotes[i].post_type === "question") {
+                    if (questionVotes[i].vote_type === "upvote") {
+                        numOfUpvotes += 1;
+                    } else if (questionVotes[i].vote_type === "downvote") {
+                        numOfDownvotes += 1
+                    }
+                }
+            }
+            return numOfUpvotes - numOfDownvotes
+        }
+
         return (
             <div>
                 <div>
@@ -108,11 +137,34 @@ class Search extends React.Component {
                             <div>
                                 {this.state.questionsThatContain.map((question,idx) => (
                                     <div key={idx} className="search-ind-question">
-                                        <Link to={`questions/${question.id}`} className="search-question-title">
-                                            {question.title}
-                                        </Link>
-                                        <div className="search-question-body">
-                                            {ReactHtmlParser(question.body)}
+                                        <div className="search-vote-answer-count">
+                                            <div className="search-vote-count-word-container">
+                                                <div className="search-vote-count">
+                                                    {countOfVotes(question.votes)}
+                                                </div>
+                                                <div className="search-vote-word">
+                                                    votes
+                                                </div>
+                                            </div>
+                                            <div className="search-answer-count-word-container">
+                                                <div className="search-answer-count">
+                                                    {question.countOfAnswers}
+                                                </div>
+                                                <div className="search-answer-word">
+                                                    answers
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="search-question-title-body-wrapper">
+                                            <Link to={`questions/${question.id}`} className="search-question-title">
+                                                {question.title}
+                                            </Link>
+                                            <div className="search-question-body">
+                                                {ReactHtmlParser(question.body)}
+                                            </div>
+                                            <div>
+                                                <p>{timeAgo(question.secs)} by {question.author}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 ))}
@@ -122,11 +174,34 @@ class Search extends React.Component {
                                 <div key={idx} className="search-ind-question">
                                     {(question.title.includes(this.state.searchFor)) ?
                                     (<div>
-                                        <Link to={`questions/${question.id}`} className="search-question-title">
-                                            {question.title}
-                                        </Link>
-                                        <div className="search-question-body">
-                                            {ReactHtmlParser(question.body)}
+                                        <div className="search-vote-answer-count">
+                                            <div className="search-vote-count-word-container">
+                                                <div className="search-vote-count">
+                                                    {countOfVotes(question.votes)}
+                                                </div>
+                                                <div className="search-vote-word">
+                                                    votes
+                                                </div>
+                                            </div>
+                                            <div className="search-answer-count-word-container">
+                                                <div className="search-answer-count">
+                                                    {question.countOfAnswers}
+                                                </div>
+                                                <div className="search-answer-word">
+                                                    answers
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="search-question-title-body-wrapper">
+                                            <Link to={`questions/${question.id}`} className="search-question-title">
+                                                {question.title}
+                                            </Link>
+                                            <div className="search-question-body">
+                                                {ReactHtmlParser(question.body)}
+                                            </div>
+                                            <div>
+                                                <p>{timeAgo(question.secs)} by {question.author}</p>
+                                            </div>
                                         </div>
                                     </div>) : null}
                                 </div>
